8761154e14a1c0945d4db0358e29d45624bedc16
remi <remi3195@gmail.com>
2026-04-30 16:05:59 +0200

    fix spotify name

--- code/service-hyprland.c
+++ code/service-hyprland.c
@@ -50,7 +50,7 @@ static long string_application(const char* name)
 	if (string_cmp(name, "firefox")) return 1;
 	if (string_cmp(name, "vesktop")) return 2;
 	if (string_cmp(name, "steam")) return 3;
-	if (string_cmp(name, "spotify")) return 4;
+	if (string_cmp(name, "Spotify")) return 4;
 	if (string_cmp(name, "blender")) return 5;
 	if (string_cmp(name, "krita")) return 6;
 	if (string_cmp(name, "osu!")) return 7;
